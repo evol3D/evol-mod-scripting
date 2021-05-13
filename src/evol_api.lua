cae828060f05d8712744ed64f592aded6fd07f67
Robear Selwans <robear.selwans@outlook.com>
2021-05-13 14:30:16 +0200

    Updated main scripting API
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- src/evol_api.lua
+++ src/evol_api.lua
@@ -14,6 +14,20 @@ function Vec3:new(nx, ny, nz)
   return out
 end
 
+Vec3.__add = function(v1, v2)
+  return Vec3:new(
+    v1.x + v2.x,
+    v1.y + v2.y,
+    v1.z + v2.z)
+end
+
+Vec3.__sub = function(v1, v2)
+  return Vec3:new(
+    v1.x - v2.x,
+    v1.y - v2.y,
+    v1.z - v2.z)
+end
+
 Vec4 = {}
 function Vec4:new(nx, ny, nz, nw)
   -- creating an instance of the class
@@ -30,6 +44,21 @@ function Vec4:new(nx, ny, nz, nw)
   return out
 end
 
+Vec4.__add = function(v1, v2)
+  return Vec4:new(
+    v1.x + v2.x,
+    v1.y + v2.y,
+    v1.z + v2.z,
+    v1.w + v2.w)
+end
+
+Vec4.__sub = function(v1, v2)
+  return Vec4:new(
+    v1.x - v2.x,
+    v1.y - v2.y,
+    v1.z - v2.z,
+    v1.w - v2.w)
+end
 
 
 
@@ -39,10 +68,10 @@ end
 -- Entity Stuff
 
 Entities = {}
-
 Entity = {}
-
 ComponentGetters = {}
+EntityMemberGetters = {}
+EntityMemberSetters = {}
 
 function Entity:getID()
   return self.entityID
@@ -55,7 +84,7 @@ end
 function Entity:new(id)
   new = {}
   setmetatable(new, self)
-  self.__index = self
+  -- self.__index = self
 
   new:setID(id)
 
@@ -66,4 +95,20 @@ end
 
 function Entity:getComponent(cmp)
   return ComponentGetters[cmp](self.entityID)
+end
+
+function Entity:__index(key)
+  if EntityMemberGetters[key] == nil then
+    return rawget(self, key) or rawget(getmetatable(self), key)
+  else
+    return EntityMemberGetters[key](self.entityID)
+  end
+end
+
+function Entity:__newindex(key, val)
+  if EntityMemberSetters[key] == nil then
+    rawset(self, key, val)
+  else
+    EntityMemberSetters[key](self.entityID, val)
+  end
 end
